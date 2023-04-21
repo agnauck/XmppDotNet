e65ce16a8dbeefcc43d611033448e5f739905c33
Alexander Gnauck <gnauck@gmail.com>
2023-04-21 21:46:02 +0200

    add flip-pae to MAM

--- src/XmppDotNet.Core/Xmpp/MessageArchiveManagement/MessageArchive.cs
+++ src/XmppDotNet.Core/Xmpp/MessageArchiveManagement/MessageArchive.cs
@@ -33,5 +33,27 @@ namespace XmppDotNet.Xmpp.MessageArchiveManagement
             get => GetAttribute("node");
             set => SetAttribute("node", value);
         }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the order of the results should
+        /// be in normal or reverse order.
+        /// When fetching a page, the client may prefer for the server to send
+        /// the results within that page in reverse order. For example, if a client
+        /// implements a user interface that automatically fetches older messages
+        /// as a user scrolls backward, it may want to receive and display the newest
+        /// messages first, instead of waiting for the whole page to be received.
+        /// </summary>
+        /// <value><c>true</c> if flipped (reverse); otherwise, <c>false</c>.</value>
+        public bool FlipPage
+        {
+            get => HasTag("flip-page");
+            set
+            {
+                if (value)
+                    SetTag("flip-page");
+                else
+                    RemoveTag("flip-page");
+            }
+        }
     }
 }
